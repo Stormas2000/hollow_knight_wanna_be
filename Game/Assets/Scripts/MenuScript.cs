fdf61f8060ab12d5cfa0031906ee1bbaec4aa10e
naro3000 <diogo.fontes.alves@gmail.com>
2018-10-26 00:49:02 +0100

    Update

---
+++ Game/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+    public void Menu(int a)
+    {
+        if (a == 1){
+
+        }else if (a == 2){
+
+        }else if (a == 3){
+            Application.Quit();
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+}
