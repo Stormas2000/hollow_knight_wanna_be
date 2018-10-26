b0c677b6785ed246fdd68dcf19035a287a22b2e4
naro3000 <diogo.fontes.alves@gmail.com>
2018-10-27 00:47:36 +0100

    Settings

---
+++ Game/Assets/Scripts/SettingMenuScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SettingMenuScript : MonoBehaviour {
+
+    Resolution[] res;
+
+    public Dropdown resDropdown;
+
+    void Start()
+    {
+        res = Screen.resolutions;
+        resDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int ResolutionIndex = 0;
+        for (int i = 0; i < res.Length; i++)
+        {
+            string option = res[i].width + "x" + res[i].height;
+            options.Add(option);
+
+            if ((res[i].width == Screen.currentResolution.width) && (res[i].height == Screen.currentResolution.height))
+            {
+                ResolutionIndex = i;
+            }
+        }
+
+        resDropdown.AddOptions(options);
+        resDropdown.value = ResolutionIndex;
+        resDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int i)
+    {
+        Resolution resolution = res[i];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume(float volume)
+    {
+       
+    }
+
+    public void SetQuality(int i)
+    {
+        QualitySettings.SetQualityLevel(i);
+    }
+
+    public void SetFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
