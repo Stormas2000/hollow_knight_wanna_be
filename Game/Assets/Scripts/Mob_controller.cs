467e1df65cc29ab1137a15847584c2f7beaf36b9
Stomas2000 <tiagomoises00@gmail.com>
2018-10-27 22:48:38 +0100

    27/10

--- Game/Assets/Scripts/Mob_controller.cs
+++ Game/Assets/Scripts/Mob_controller.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Mob_controller : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    bool offensivemode;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void FixedUpdate()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (gameObject.tag == "Player")
+        {
+            offensivemode = false;
+        }
+    }
 }
