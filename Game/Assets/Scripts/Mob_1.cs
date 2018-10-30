73ff2a8fa252fac723cc9f38b59d8af187d2273d
Stomas2000 <tiagomoises00@gmail.com>
2018-10-30 21:40:17 +0000

    30/10/2018

---
+++ Game/Assets/Scripts/Mob_1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mob_1 : MonoBehaviour {
+    bool offensivemode;
+    private Rigidbody2D rigi_mob1;
+    public float speed, jump,health;
+    public int attack_damage;
+    // Use this for initialization
+    void Start () {
+        health = 100;
+        attack_damage = 10;
+	}
+	
+	// Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (offensivemode == true)
+        {
+            speed = 10;
+            offensivemode = false;
+        }
+        rigi_mob1.velocity = new Vector2(speed, jump);
+        jump = 0;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            offensivemode = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag != "Player" || collision.gameObject.tag != "Weapon")
+        {
+            jump = 25;
+        }
+        if(collision.gameObject.tag != "Weapon")
+        {
+            //chamar a variavel do player q e responsavel pelo damage
+        }
+    }
+}
---
+++ Game/Assets/Scripts/Player_manegement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_manegement : MonoBehaviour {
+    public float Player_hp;
+    public int Player_base_damage, monster_damage, armor,level,experience, experience_level_up;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void Level_manager()
+    {
+        if (experience == 100)
+        {
+            level++;
+            //aumentar a experiencia necessaria para dar level up
+        }
+    }
+    private void damage_manager()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag =="Monster")
+        {
+            //chamar as variaveis
+            damage_manager();
+        }
+    }
+}
